9cb57aed7a9d680094e720f766eeba9e71448174
Zegram2112 <nicolas.lemunir2112@gmail.com>
2017-11-20 13:23:46 -0300

    Modify import statement in apps/blog/urls.py
    Before this modification the views import was full with specific class imports.
    Now I've changed it to call the views module with 'v' from urls.py

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url
-from .views import IndexView, EntryDetailView, SearchEntryView, TagDetailView
+import views as v
 
 app_name = 'blog'
 urlpatterns = [
-    url(r'^$', IndexView.as_view(),
+    url(r'^$', v.IndexView.as_view(),
         name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', EntryDetailView.as_view(),
+    url(r'^(?P<pk>[0-9]+)/$', v.EntryDetailView.as_view(),
         name='detail'),
-    url(r'^search/$', SearchEntryView.as_view(),
+    url(r'^search/$', v.SearchEntryView.as_view(),
         name='search'),
-    url(r'^tag/(?P<pk>[0-9]+)/$', TagDetailView.as_view(),
+    url(r'^tag/(?P<pk>[0-9]+)/$', v.TagDetailView.as_view(),
         name='tag'),
 ]
